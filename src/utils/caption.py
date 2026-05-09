2ab23c0f87c5674ac0fccdfe8d2867f93572c84f
Raihan Ekp <raihanekap04@gmail.com>
2026-05-09 11:02:23 +0700

    add caption real

--- src/utils/caption.py
+++ src/utils/caption.py
@@ -26,38 +26,63 @@ def format_tiktok_caption(
     name, 
     username,
     link,
-    timestamp=None
+    timestamp=None,
+    description=None
 ):
 
     date_text = format_wib_time(
         timestamp
     )
 
+    caption_text = ""
+
+    if description:
+
+        caption_text = (
+            f"\n\n"
+            f"<blockquote>"
+            f"{escape(description)}"
+            f"</blockquote>"
+        )
+
     return (
         f"🎵 <b>TikTok Update • "
-        f"{escape(name)}</b>\n\n"
+        f"{escape(name)}</b>"
+        f"{caption_text}\n\n"
         f"👤 <code>@{escape(username)}</code>\n"
         f"🗓 {date_text}\n\n"
         f'🔗 <a href="{link}">'
-        f'Lihat postingan</a>'
+        f"Lihat postingan</a>"
     )
 
 def format_x_caption(
     name, 
     username,
     link,
-    timestamp=None
+    timestamp=None,
+    description=None
 ):
 
     date_text = format_wib_time(
         timestamp
     )
+    caption_text = ""
+
+    if description:
+
+        caption_text = (
+            f"\n\n"
+            f"<blockquote>"
+            f"{escape(description)}"
+            f"</blockquote>"
+        )
 
     return (
         f"🐦 <b>X Update • "
-        f"{escape(name)}</b>\n\n"
+        f"{escape(name)}</b>"
+        f"{caption_text}\n\n"
         f"👤 <code>@{escape(username)}</code>\n"
         f"🗓 {date_text}\n\n"
         f'🔗 <a href="{link}">'
-        f'Lihat postingan</a>'
+        f"Lihat postingan</a>"
     )
