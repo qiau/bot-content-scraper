233825433c0b97986027f6a991c7cdb2ac1ed71b
Raihan Ekp <raihanekap04@gmail.com>
2026-05-16 01:18:33 +0700

    add birthday

--- src/main_birthday.py
+++ src/main_birthday.py
@@ -1,15 +1,22 @@
 import json
+import asyncio
+import os
 from datetime import datetime
+from dotenv import load_dotenv
 
-from src.handlers.telegram_handler import _send_message
+from src.handlers.telegram_handler import init_telegram, close_telegram, _send_message
 from src.utils.caption_utils import format_birthday_caption
+
+load_dotenv()
 
 def load_targets():
     with open("data/targets.json", "r") as f:
         return json.load(f)
 
-def main():
-    
+async def main():
+
+    await init_telegram(os.getenv("TELEGRAM_TOKEN_SOCIAL"))
+
     targets = load_targets()
 
     today = datetime.now()
@@ -36,7 +43,21 @@ def main():
                 age
             )
 
-            _send_message(caption)
+            try:
+
+                await _send_message(
+                    caption,
+                    parse_mode="HTML"
+                )
+                await asyncio.sleep(5)
+
+            except Exception as e:
+                print(
+                    f"❌ Birthday error "
+                    f"{name}: {e}"
+                )
+
+    await close_telegram()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
